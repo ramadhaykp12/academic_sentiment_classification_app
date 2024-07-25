f100a2e0c294f8204caabb8607c2288e635fc8f0
Ramadhany Kurnia Pratama <88931175+ramadhaykp12@users.noreply.github.com>
2024-07-25 23:45:17 +0700

    Update web.py

--- web.py
+++ web.py
@@ -2,7 +2,7 @@ import streamlit as st
 import tensorflow as tf
 from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing.sequence import pad_sequences
-from tensorflow.keras.preprocessing.text import Tokenizer
+#from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.preprocessing.text import tokenizer_from_json
 import nltk
 from nltk.corpus import stopwords
