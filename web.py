9332dc610705555ac7712995ecfdc852cdd67e73
Ramadhany Kurnia Pratama <88931175+ramadhaykp12@users.noreply.github.com>
2024-07-26 13:48:05 +0700

    Update web.py

--- web.py
+++ web.py
@@ -19,48 +19,17 @@ with open('tokenizer.json', 'r') as f:
     tokenizer_json = json.load(f)
     tokenizer = tokenizer_from_json(tokenizer_json)
 
-# Preprocessing functions
-nltk.download('stopwords')
-nltk.download('punkt')
-stop_words = set(stopwords.words('indonesian'))
-
-def preprocess_text(text):
-    # Lowercase
-    text = text.lower()
-    # Tokenize
-    words = word_tokenize(text)
-    # Remove punctuation and stopwords
-    words = [word for word in words if word.isalnum() and word not in stop_words]
-    return words
-
-def prepare_input(text, tokenizer, max_len=100):
-    # Tokenize and pad sequences
-    sequences = tokenizer.texts_to_sequences([text])
-    padded_sequences = pad_sequences(sequences, maxlen=max_len)
-    return padded_sequences
-    
 # Streamlit app
 st.title('Aplikasi Klasifikasi Teks')
 
 input_text = st.text_input('Masukkan teks untuk klasifikasi:')
 if st.button('Klasifikasi'):
     if input_text:
-        # Preprocess text
-        preprocessed_text = preprocess_text(input_text)
-        # Prepare input for model
-        input_data = prepare_input(preprocessed_text, tokenizer)
-        # Predict
-        prediction = model.predict(input_data)
-        prediction_prob_negative = prediction[0][0]
-        prediction_prob_neutral = prediction[0][1]
-        prediction_prob_positive= prediction[0][2]
-        prediction_class = prediction.argmax(axis=-1)[0]
-        print(prediction.argmax())
-        # Display result
-        st.header('Prediction using LSTM model')
-        if prediction_class == 0:
-          st.warning('Thread has negative sentiment')
-        if prediction_class == 1:
-          st.success('Thread has neutral sentiment')
-        if prediction_class==2:
-          st.success('Thread has positive sentiment')
+    sequences = tokenizer.texts_to_sequences([input_text])
+    padded = pad_sequences(sequences, maxlen=100)
+    prediction = model.predict(padded)
+
+    sentiment_labels = ['Negative', 'Neutral', 'Positive']
+    predicted_label = sentiment_labels[np.argmax(prediction)]
+
+    st.write(f"Predicted Sentiment: **{predicted_label}**")
